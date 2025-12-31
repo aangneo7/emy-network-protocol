ef3c3455dd1662c6e8a865e5184b88782e763eec
aangneo7 <113002498+aangneo7@users.noreply.github.com>
2025-12-31 16:52:25 +0700

    Create EmyToken.sol

---
+++ contracts/EmyToken.sol
@@ -0,0 +1,21 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+/**
+ * @title EmyToken
+ * @dev Core utility token for the Emy Network ecosystem.
+ */
+ 
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract EmyToken is ERC20, Ownable {
+    constructor() ERC20("Emy Network", "EMY") Ownable(msg.sender) {
+        // Minting 1 Billion EMY tokens to the deployer
+        _mint(msg.sender, 1000000000 * 10 ** decimals());
+    }
+
+    function burn(uint256 amount) public {
+        _burn(msg.sender, amount);
+    }
+}
