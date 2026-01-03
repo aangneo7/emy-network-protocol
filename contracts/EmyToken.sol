6b9174ba69b8c0a2ba9a9c44589c547dd87af487
aangneo7 <113002498+aangneo7@users.noreply.github.com>
2026-01-03 16:32:35 +0700

    Update EmyToken.sol

--- contracts/EmyToken.sol
+++ contracts/EmyToken.sol
@@ -1,22 +1,27 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.20;
+pragma solidity ^0.8.0;
 
-/**
- * @title EmyToken
- * @dev Core utility token for the Emy Network ecosystem.
- */
- 
-import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
-import "@openzeppelin/contracts/access/Ownable.sol";
+contract EmyToken {
+    string public name = "Emy Network Token";
+    string public symbol = "EMY";
+    uint8 public decimals = 18;
+    uint256 public totalSupply;
 
-contract EmyToken is ERC20, Ownable {
-    constructor() ERC20("Emy Network", "EMY") Ownable(msg.sender) {
-        // Minting 1 Billion EMY tokens to the deployer
-        _mint(msg.sender, 1000000000 * 10 ** decimals());
+    mapping(address => uint256) public balanceOf;
+
+    event Transfer(address indexed from, address indexed to, uint256 value);
+
+    constructor(uint256 _initialSupply) {
+        totalSupply = _initialSupply * 10 ** uint256(decimals);
+        balanceOf[msg.sender] = totalSupply;
     }
 
-    function burn(uint256 amount) public {
-        _burn(msg.sender, amount);
+    function transfer(address _to, uint256 _value) public returns (bool success) {
+        require(balanceOf[msg.sender] >= _value, "Balance insufficient");
+        balanceOf[msg.sender] -= _value;
+        balanceOf[_to] += _value;
+        emit Transfer(msg.sender, _to, _value);
+        return true;
     }
 }
 
